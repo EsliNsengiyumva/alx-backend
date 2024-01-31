b322c3b6f046658848b8b4203ea6682cf22bab41
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2024-01-31 12:28:41 +0200

    commit files

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,64 +1,45 @@
-#!/usr/bin/python3
-""" Base class for all"""
+#!/usr/bin/env python3
+"""
+caching System
+"""
 
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
     """
-    Implementation of LFU (Least Frequently Used) caching algorithm with LRU tie-breaking.
+    caching system:
+
+    Args:
+        LFUCache ([class]): [basic caching]
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """ initialize of class """
+        self.temp_list = {}
         super().__init__()
-        self.frequency_counter = {}
 
     def put(self, key, item):
         """
-        Add an item to the cache.
-
-        Args:
-            key: The key for the item.
-            item: The item to be cached.
+        Add an item in the cache
         """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                # If the key exists, update the frequency
-                self.frequency_counter[key] += 1
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = min(self.temp_list, key=self.temp_list.get)
+                self.temp_list.pop(pop)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+            if not (key in self.temp_list):
+                self.temp_list[key] = 0
             else:
-                # If the key is new, set frequency to 1
-                self.frequency_counter[key] = 1
-
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Discard the least frequency used item (LFU algorithm)
-                least_frequent_keys = [k for k, v in self.frequency_counter.items() if v == min(self.frequency_counter.values())]
-                if len(least_frequent_keys) > 1:
-                    # Use LRU algorithm to break the tie
-                    lfu_key = min(least_frequent_keys, key=lambda k: self.cache_data[k]['timestamp'])
-                else:
-                    lfu_key = least_frequent_keys[0]
-
-                del self.cache_data[lfu_key]
-                del self.frequency_counter[lfu_key]
-                print("DISCARD:", lfu_key)
-
-            # Update the cache with the new item
-            self.cache_data[key] = {'value': item, 'timestamp': self.timestamp}
-            self.timestamp += 1
+                self.temp_list[key] += 1
 
     def get(self, key):
         """
-        Retrieve an item from the cache.
-
-        Args:
-            key: The key of the item to be retrieved.
-
-        Returns:
-            The cached item, or None if the key is not in the cache.
+        Get an item by key
         """
-        if key is not None and key in self.cache_data:
-            # Update the frequency and timestamp
-            self.frequency_counter[key] += 1
-            self.cache_data[key]['timestamp'] = self.timestamp
-            return self.cache_data[key]['value']
-        return None
+        if (key is None) or not (key in self.cache_data):
+            return None
+        self.temp_list[key] += 1
+        return self.cache_data.get(key)
